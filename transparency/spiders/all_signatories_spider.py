4991d15d536b921fa05bc23559bf7437e540e302
Amit Kumar Dubey <guttume@outlook.com>
2020-04-02 15:25:08 +0530

    updated all signatories spider to include all details

--- transparency/spiders/all_signatories_spider.py
+++ transparency/spiders/all_signatories_spider.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
+from transparency.parsers.signatory_detail import Parser
+
 
 class AllSignatoriesSpider(scrapy.Spider):
     name = 'AllSignatories'
@@ -11,12 +13,5 @@ class AllSignatoriesSpider(scrapy.Spider):
         yield from response.follow_all(signatory_page_links, self.details)
 
     def details(self, response):
-        fund_management = response.css('tr#32f3bde967894662b11458d1c48d0a2d label.radio img[src="/Style/img/checkedradio.png"]+span.title b::text').get('NA')
-        sub_advised_products = response.css('tr#336bca6d619f473d853ec277caed8e8a label.radio img[src="/Style/img/checkedradio.png"]+span.title b::text').get('NA')
-        others = response.css('tr#d896a5424372407589eeddc43843cd5e label.radio img[src="/Style/img/checkedradio.png"]+span.title b::text').get('NA')
-        yield {
-            'Name': response.css('div.row-fluid div.span12 h2.n-h2::text').get(),
-            'Fund management': fund_management.strip(),
-            'Fund of funds, manager of managers, sub-advised products': sub_advised_products.strip(),
-            'others': others.strip(),
-        }
+        parser = Parser(response)
+        yield parser.parse_details()
